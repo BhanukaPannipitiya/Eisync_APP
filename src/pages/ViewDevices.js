c46e1513e87e5b1b28150ae32d153525087a227e
Bhanuka69 <103242662+Bhanuka69@users.noreply.github.com>
2023-11-24 10:24:54 +0530

    View Devices

--- src/pages/ViewDevices.js
+++ src/pages/ViewDevices.js
@@ -1,35 +1,71 @@
-import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import { heightPercentageToDP } from "react-native-responsive-screen";
+import { Ionicons } from "@expo/vector-icons";
 
 const ViewDevices = () => {
-  const devices = [
-    { name: "TV", voltage: "20v", power: "4W" },
-    { name: "AC", voltage: "20v", power: "7W" },
-    { name: "Oven", voltage: "4v", power: "6W" },
-    { name: "20v 4W", voltage: "5v", power: "5W" },
-    { name: "Washing machine", voltage: "20V", power: "4W" },
-    { name: "Fan", voltage: "20v", power: "8W" },
-    { name: "Laptop", voltage: "20V", power: "1W" },
-    { name: "Microwave", voltage: "20v", power: "2W" },
-    { name: "Trimmer", voltage: "20v", power: "4W" },
-  ];
+  const [appliances, setAppliances] = useState([]);
 
-  const renderItem = ({ item }) => (
-    <View style={styles.deviceContainer}>
-      <Text style={styles.deviceName}>{item.name}</Text>
-      <Text style={styles.deviceVoltage}>{item.voltage}</Text>
-      <Text style={styles.devicePower}>{item.power}</Text>
-      <Text style={styles.deviceCost}>{item.cost}</Text>
-    </View>
-  );
+  useEffect(() => {
+    fetchAppliances();
+  }, []);
+
+  const fetchAppliances = async () => {
+    try {
+      const response = await fetch(
+        "http://192.168.8.164:3000/getAllAppliances"
+      );
+      const data = await response.json();
+      setAppliances(data);
+    } catch (error) {
+      console.error("Error fetching appliances:", error);
+    }
+  };
+
+  const renderItem = ({ item, index }) => {
+    if (index === 0){
+      return (
+        <TouchableOpacity style={styles.addDevice} key="addButton">
+        <Ionicons name="add-circle-outline" size={100} color="#4ECCA3" />  
+      </TouchableOpacity>
+      )
+    }
+    
+
+    return (
+     
+      <View style={styles.deviceContainer} key={item._id}>
+        <Text style={styles.deviceName}>{item.deviceType}</Text> 
+        <Text style={styles.deviceVoltage}>{item.voltage}</Text>
+        <Text style={styles.devicePower}>{item.power}</Text>
+      </View>
+    );
+  };
 
   return (
     <View style={styles.container}>
+      <View style={styles.headingContainer}>
+        <Text style={styles.heading}>
+          Your <Text style={styles.subHeading}>Devices</Text>
+        </Text>
+      </View>
+     
+      {appliances.length === 0 ? (
+        <View style={styles.noDevicesContainer}>
+          <Text style={styles.noDevicesText}>No devices found</Text>
+        </View>
+      ) : null}
+
       <FlatList
-        data={devices}
+        data={appliances}
         renderItem={renderItem}
-        keyExtractor={(item) => item.name}
+        keyExtractor={(item) => item._id}
         numColumns={2}
       />
     </View>
@@ -37,37 +73,92 @@ const ViewDevices = () => {
 };
 
 const styles = StyleSheet.create({
+  headingContainer: {
+    alignItems: "flex-start",
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  heading: {
+    fontSize: 26,
+    fontWeight: "normal",
+    fontFamily: "Poppins",
+    color: "#000000",
+  },
+  subHeading: {
+    color: "#4ECCA3",
+  },
   container: {
     flex: 1,
     paddingTop: 22,
+  },
+  addDevice: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 20,
+    padding: 10,
+    borderColor: "#000",
+    height: heightPercentageToDP(20),
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+    elevation: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  noDevicesContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  noDevicesText: {
+    fontSize: 18,
+    color: "#000",
   },
   deviceContainer: {
     flex: 1,
     marginLeft: 10,
     marginRight: 10,
-    marginBottom: 20, // Adjust the marginBottom to create space between rows
+    marginBottom: 20,
     padding: 10,
     backgroundColor: "#186049",
     borderColor: "#000",
     height: heightPercentageToDP(20),
+    borderRadius: 10,
+    backgroundColor: "#186049",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+    elevation: 5,
   },
   deviceName: {
     fontSize: 16,
     marginBottom: 4,
+    color: "#FFF",
   },
   deviceVoltage: {
     fontSize: 14,
-    color: "#666",
+    color: "#FFF",
     marginBottom: 4,
   },
   devicePower: {
     fontSize: 14,
-    color: "#666",
+    color: "#FFF",
     marginBottom: 4,
   },
   deviceCost: {
     fontSize: 14,
-    color: "#666",
+    color: "#FFF",
   },
 });
 
